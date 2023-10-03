4b9cc76f449bf4c56f2c81f0b931b9456e333196
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-03 05:53:03 -0700

    requirement

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
         number = abs(number)
     l_di = number % 10
-    print(l_di, end = "")
+    print("{}".format(l_di), end="")
     return l_di
