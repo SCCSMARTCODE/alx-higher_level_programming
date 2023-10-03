96b0cc9d154dd7ea33d9abbb6d6992a111771e10
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-03 05:16:24 -0700

    requirement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -7,9 +7,9 @@ while val <= 9:
     while val2 <= 9:
         if val2 > val:
             if val != 8:
-                print(f"{val}{val2}", end = ", ")
+                print("{}{}".format(val, val2), end=", ")
             else:
-                print(f"{val}{val2}")
+                print("{}{}".format(val, val2))
         val2 += 1
     val2 = 0
     val += 1
