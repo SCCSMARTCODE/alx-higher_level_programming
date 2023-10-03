5e953ffe7b49c0b06c9c40a14568875e59b19809
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-03 05:34:57 -0700

    requirement

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def islower(c):
+
+    if c != str(c):
+        return Error 
     ch = 'a'
     while ch <= 'z':
         if c == ch:
             return True
         ch = chr(ord(ch) + 1)
-        
+
     return False
