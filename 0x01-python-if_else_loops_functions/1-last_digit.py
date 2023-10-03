2e2d6e8bf43886f0906ca3606db60f53fd3f2c80
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-03 04:30:59 -0700

    negative issue solved

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 l_digit = number % 10
-print("Last digit of {} is {} and is".format(number, l_digit), end = ' ')
+
+if number < 0:
+    number1 = abs(number)
+    l_digit = number1 % 10
+    l_digit *= -1
+
+print("Last digit of {} is {} and is".format(number, l_digit), end=' ')
 if l_digit > 5:
     print("greater than 5")
 elif l_digit == 0:
