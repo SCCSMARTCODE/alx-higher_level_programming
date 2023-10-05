4447be7193c8eafc7efbad7e21df2abee6981a03
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-05 05:34:05 -0700

    check Error

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,10 +4,10 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-add_val = cal.add(a, b)
-sub_val = cal.sub(a, b)
-mul_val = cal.mul(a, b)
-div_val = cal.div(a, b)
+add_val = add(a, b)
+sub_val = sub(a, b)
+mul_val = mul(a, b)
+div_val = div(a, b)
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add_val))
     print("{} - {} = {}".format(a, b, sub_val))
