270bfdf1b6be8f3a9a6f4f45fd8b24e5eaa431fe
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-05 05:32:20 -0700

    check2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import calculator_1 as cal
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
@@ -8,9 +8,9 @@ add_val = cal.add(a, b)
 sub_val = cal.sub(a, b)
 mul_val = cal.mul(a, b)
 div_val = cal.div(a, b)
-
-print("{} + {} = {}".format(a, b, add_val))
-print("{} - {} = {}".format(a, b, sub_val))
-print("{} * {} = {}".format(a, b, mul_val))
-print("{} / {} = {}".format(a, b, div_val))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add_val))
+    print("{} - {} = {}".format(a, b, sub_val))
+    print("{} * {} = {}".format(a, b, mul_val))
+    print("{} / {} = {}".format(a, b, div_val))
 
