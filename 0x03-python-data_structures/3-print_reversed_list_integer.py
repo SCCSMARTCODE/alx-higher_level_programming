afe8540b248abb96fcc33197174e8d696c91f779
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-07 02:56:36 -0700

    bugson requirement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    i = -1
-    for x in my_list:
-        print("{:d}".format(my_list[i]))
-        i -= 1
+    for x in reversed(my_list):
+        print("{:d}".format(x))
