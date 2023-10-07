e1fd6b9d861cdfbf1a424538b556f816dd4d879c
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-07 03:51:09 -0700

    error checked

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     ans = []
     for x in my_list:
         if x % 2 == 0:
-            ans += [True]
+            ans.append(True)
         else:
-            ans += [False]
+            ans.append(False)
     return(ans)
