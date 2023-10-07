1de57120bf812938ef4219deade6140c6f24c0e4
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-07 03:16:23 -0700

    bugson fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if 0 > idx > (len(my_list) - 1):
+    if 0 > idx or idx > (len(my_list) - 1):
         return my_list
     else:
         n_list = my_list.copy()
