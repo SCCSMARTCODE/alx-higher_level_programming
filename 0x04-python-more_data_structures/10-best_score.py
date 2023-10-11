ca5d64d23b7e7d79deaf1add5abc1d0668d2e312
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-11 07:19:49 -0700

    pastint the right codes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict)
-        return max(a_dictionary, key=a_dictionary.get, default=None)
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
