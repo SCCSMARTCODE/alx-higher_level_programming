b145390c60539c45b448877929807c631daf0782
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-11 07:26:14 -0700

    pastint the right codes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-    new = list(my_list)
-    # remove repetition in list
-    for i in new:
-        j = 0
-        ne = i
-        for i in new:
-            if i == ne:
-                del new[j]
-            j += 1
-        new.append(ne)
-    sum = 0
-    for x in new:
-        sum += x
-    return sum
+    unique_set = set()
+    total = 0
+
+    for num in my_list:
+        if num not in unique_set:
+            total += num
+            unique_set.add(num)
+
+    return total
