40404775c2606c4df57f3ae0a0057801e238adfa
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-24 15:36:37 -0700

    ######gi!

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,13 +6,13 @@ This module defines a simple Square class that can be used to
 represent squares in your Python programs.
 
 Usage Example:
-    
+
     # Import the Square class
     Square = __import__('#-square').Square
-    
+
     # Create an instance of the Square class
     my_square = Square()
-    
+
     # Display the type of the object and its dictionary
     print(f"Type of my_square: {type(my_square)}")
     print(f"Dictionary of my_square: {my_square.__dict__}")
