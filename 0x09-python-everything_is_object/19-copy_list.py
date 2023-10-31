43513ab1f491d720127621a96d7af57e8dbb2b67
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-31 13:43:06 -0700

    ###########

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is a module that help to copy a list with perfect similarity
+testing:
+    copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+"""
+def copy_list(l):
+    """Working on
+        the copy_list_function
+    Return:
+        l[:]
+    """
+    return l[:]
