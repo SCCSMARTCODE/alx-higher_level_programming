922baa40f95bf6775b768d3927c38478590aea2a
SCCSMARTCODE <sccsmart247@gmail.com>
2023-11-07 14:20:55 -0800

    ##

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains 
+     a function that writes a string to a text file (UTF8) 
+"""
+
+
+def write_file(filename="", text=""):
+    """This module Write a function that writes a string
+            to a text file (UTF8)
+        Return:
+             the number of characters written:
+             """
+    with open(filename, 'w') as f:
+        return f.write(text)
