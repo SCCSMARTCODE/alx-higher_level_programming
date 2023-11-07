0ade99c4b7ccbbfd35126b5a44f82eb974a8e650
SCCSMARTCODE <sccsmart247@gmail.com>
2023-11-07 14:59:02 -0800

    ##

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 8-class_to_json.
+Returns the dictionary description
+"""
+
+
+def class_to_json(obj):
+    """Creates a dict description of obj.
+
+    Args:
+            ...
+    """
+
+    return obj.__dict__
