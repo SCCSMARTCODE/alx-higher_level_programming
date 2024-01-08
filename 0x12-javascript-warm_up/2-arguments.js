e765a8bb95f3e98c8d5296bf7707f362a38e4137
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-08 13:32:20 -0800

    run

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const numArgs = process.argv.length - 2;
 
 if (numArgs === 0) {
-  console.log('No argumen'");
+  console.log('No argument');
 } else if (numArgs === 1) {
   console.log('Argument found');
 } else {
